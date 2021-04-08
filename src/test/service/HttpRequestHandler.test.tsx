47087d32fc3c682146c6fe549f45f757842e7978
a_assis_ext <a.assis.ext@planerio.de>
2021-04-08 15:10:16 -0300

    clean test file

--- src/test/service/HttpRequestHandler.test.tsx
+++ src/test/service/HttpRequestHandler.test.tsx
@@ -2,20 +2,18 @@ import axios, { AxiosResponse } from "axios";
 import { handleRequest} from '../../service/HttpRequestHandler'
 
 //https://stackoverflow.com/questions/56977919/how-to-mock-axios-when-passed-a-config-w-typescript
-
+//reference: https://stackoverflow.com/questions/51275434/cannot-get-jest-typescript-axios-test
+//reference: https://medium.com/swlh/mock-a-library-in-jest-and-typescript-a8bec189efc3
 //good article: https://www.robinwieruch.de/axios-jest
+//https://stackoverflow.com/questions/47144187/can-you-write-async-tests-that-expect-tothrow
+//https://jestjs.io/pt-BR/docs/asynchronous
 describe("http handler service", () => {
 
     test("mock axios client and retrieve success data", async () => {
         
         const mockedAxios = axios as jest.Mocked<typeof axios>;
-        
-        // const mockedData = {test:"123"};
-        //reference: https://stackoverflow.com/questions/51275434/cannot-get-jest-typescript-axios-test
-        //reference: https://medium.com/swlh/mock-a-library-in-jest-and-typescript-a8bec189efc3
-        
-        const mockedHttpResponseData = 'fake data';
 
+        const mockedHttpResponseData = 'fake data';
         const axiosResponse: AxiosResponse = {
             data: mockedHttpResponseData,
             status: 200,
@@ -34,7 +32,6 @@ describe("http handler service", () => {
         
         const actual = await handleRequest({});
         expect(actual).toBe("fake data");
-        // await expect(handleRequest({})).resolves.toEqual("mockedData");
 
     });
 
@@ -42,20 +39,6 @@ describe("http handler service", () => {
         
         const mockedAxios = axios as jest.Mocked<typeof axios>;
         
-        // const mockedData = {test:"123"};
-        //reference: https://stackoverflow.com/questions/51275434/cannot-get-jest-typescript-axios-test
-        //reference: https://medium.com/swlh/mock-a-library-in-jest-and-typescript-a8bec189efc3
-        
-        const mockedHttpResponseData = 'fake data';
-
-        const axiosResponse: AxiosResponse = {
-            data: mockedHttpResponseData,
-            status: 200,
-            statusText: 'OK',
-            config: {},
-            headers: {},
-          };
-
         mockedAxios.create = jest.fn((config) => {
             const wrapFunction = (param:any) => {
              return Promise.reject(new Error('Network Error'))
@@ -66,9 +49,6 @@ describe("http handler service", () => {
         
          
          const expectedResult = await expect(handleRequest({}));
-         //https://stackoverflow.com/questions/47144187/can-you-write-async-tests-that-expect-tothrow
-         //https://jestjs.io/pt-BR/docs/asynchronous
-
          expectedResult.rejects.toThrowError("Network Error");
 
     });
